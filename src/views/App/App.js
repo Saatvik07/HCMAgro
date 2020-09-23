dc536bacfe2cbaedefb5a5acb8d566cd135ac84b
Saatvik07 <saatvik19097@iiitd.ac.in>
2020-09-23 09:21:16 +0530

    Footer made

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Home from "../Home/Home";
 import Products from "../Products/Products";
 import About from "../About/About";
-import { Button } from "@material-ui/core";
+import {
+	Button,
+	TableBody,
+	TableCell,
+	TableHead,
+	Table,
+	TableRow,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
+	TextField,
+} from "@material-ui/core";
 import Category1 from "../Products/Category1/Category1";
+import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
+import TripOriginIcon from "@material-ui/icons/TripOrigin";
+import EmailIcon from "@material-ui/icons/Email";
 function App() {
+	const [email, setEmail] = useState("");
 	return (
 		<div>
 			<BrowserRouter>
@@ -16,41 +31,172 @@ function App() {
 					<Route path='/products' component={Products}></Route>
 					<Route path='/category1' component={Category1}></Route>
 				</Switch>
-				{/* <footer>
-				<div className='footer-container'>
-					<div className='footer-left'>
-						<img
-							src='https://hcmagroproducts.com/wp-content/uploads/2018/08/LOgo.png'
-							alt='logo'
-							className='footer-logo'
-						></img>
-						<h6>© 2020 HCM Agro Products Pvt Ltd. All Rights Reserved.</h6>
-					</div>
-					<div className='footer-center'>
-						<Link to='/'>
-							<h4 className='footer-link'>Home</h4>
-						</Link>
+				<footer>
+					<div className='footer-container'>
+						<div className='footer-left'>
+							<img
+								src='https://hcmagroproducts.com/wp-content/uploads/2018/08/LOgo.png'
+								alt='logo'
+								className='footer-logo'
+							></img>
+							<h5>© 2020 HCM Agro Products Pvt Ltd. All Rights Reserved.</h5>
+						</div>
+						<div className='footer-center'>
+							<div className='footer-center-left'>
+								<Table>
+									<TableHead>
+										<TableRow>
+											<TableCell className='footer-tableCell'>
+												<ListItem className='footer-table-listItem'>
+													<ListItemIcon className='footer-table-icon'>
+														<TripOriginIcon />
+													</ListItemIcon>
+													<ListItemText
+														primary='Quick Links'
+														className='footer-link'
+													></ListItemText>
+												</ListItem>
+											</TableCell>
+										</TableRow>
+									</TableHead>
+									<TableBody>
+										<TableRow>
+											<TableCell className='footer-tableCell'>
+												<Link to='/' className='router-link'>
+													<ListItem className='footer-table-listItem'>
+														<ListItemIcon className='footer-table-icon'>
+															<DoubleArrowIcon />
+														</ListItemIcon>
+														<ListItemText primary='Home' className='footer-link'></ListItemText>
+													</ListItem>
+												</Link>
+											</TableCell>
+											<TableCell className='footer-tableCell'>
+												<Link to='/about' className='router-link'>
+													<ListItem className='footer-table-listItem'>
+														<ListItemIcon className='footer-table-icon'>
+															<DoubleArrowIcon />
+														</ListItemIcon>
+														<ListItemText primary='About Us' className='footer-link'></ListItemText>
+													</ListItem>
+												</Link>
+											</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell className='footer-tableCell'>
+												<Link to='/catalogue' className='router-link'>
+													<ListItem className='footer-table-listItem'>
+														<ListItemIcon className='footer-table-icon'>
+															<DoubleArrowIcon />
+														</ListItemIcon>
+														<ListItemText
+															primary='Catalogue'
+															className='footer-link'
+														></ListItemText>
+													</ListItem>
+												</Link>
+											</TableCell>
+										</TableRow>
+									</TableBody>
+								</Table>
+							</div>
+							<div className='footer-center-right'>
+								<Table>
+									<TableHead>
+										<TableRow>
+											<TableCell className='footer-tableCell'>
+												<ListItem className='footer-table-listItem'>
+													<ListItemIcon className='footer-table-icon'>
+														<TripOriginIcon />
+													</ListItemIcon>
+													<ListItemText primary='Products' className='footer-link'></ListItemText>
+												</ListItem>
+											</TableCell>
+										</TableRow>
+									</TableHead>
+									<TableBody>
+										<TableRow>
+											<TableCell className='footer-tableCell'>
+												<Link to='/category1' className='router-link'>
+													<ListItem className='footer-table-listItem'>
+														<ListItemIcon className='footer-table-icon'>
+															<DoubleArrowIcon />
+														</ListItemIcon>
+														<ListItemText
+															primary='Category1'
+															className='footer-link'
+														></ListItemText>
+													</ListItem>
+												</Link>
+											</TableCell>
+											<TableCell className='footer-tableCell'>
+												<Link to='/category2' className='router-link'>
+													<ListItem className='footer-table-listItem'>
+														<ListItemIcon className='footer-table-icon'>
+															<DoubleArrowIcon />
+														</ListItemIcon>
+														<ListItemText
+															primary='Category2'
+															className='footer-link'
+														></ListItemText>
+													</ListItem>
+												</Link>
+											</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell className='footer-tableCell'>
+												<Link to='/category3' className='router-link'>
+													<ListItem className='footer-table-listItem'>
+														<ListItemIcon className='footer-table-icon'>
+															<DoubleArrowIcon />
+														</ListItemIcon>
+														<ListItemText
+															primary='Category3'
+															className='footer-link'
+														></ListItemText>
+													</ListItem>
+												</Link>
+											</TableCell>
+											<TableCell className='footer-tableCell'>
+												<Link to='/category4' className='router-link'>
+													<ListItem className='footer-table-listItem'>
+														<ListItemIcon className='footer-table-icon'>
+															<DoubleArrowIcon />
+														</ListItemIcon>
+														<ListItemText
+															primary='Category4'
+															className='footer-link'
+														></ListItemText>
+													</ListItem>
+												</Link>
+											</TableCell>
+										</TableRow>
+									</TableBody>
+								</Table>
+							</div>
+						</div>
+						<div className='footer-right'>
+							<ListItem>
+								<ListItemIcon>
+									<EmailIcon style={{ color: "white" }} />
+								</ListItemIcon>
+								<ListItemText primary='Get the Catalogue' />
+							</ListItem>
 
-						<Link to='/about'>
-							<h4 className='footer-link'>About Us</h4>
-						</Link>
-						<Link to='/products'>
-							<h4 className='footer-link'>Products</h4>
-						</Link>
-						<Link to='/catalogue'>
-							<h4 className='footer-link'>Catalogue</h4>
-						</Link>
+							<TextField
+								className='footer-input'
+								variant='outlined'
+								label='Email'
+								onChange={(event) => {
+									setEmail(event.target.value);
+								}}
+							/>
+							<Button variant='outlined' className='footer-sendBtn'>
+								Send
+							</Button>
+						</div>
 					</div>
-					<div className='footer-right'>
-						<Button
-							variant='outlined'
-							style={{ color: "#fec397", border: "2px solid #fec397", margin: "10px" }}
-						>
-							Contact Us
-						</Button>
-					</div>
-				</div>
-			</footer> */}
+				</footer>
 			</BrowserRouter>
 		</div>
 	);
